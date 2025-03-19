0fe7eb8c4881299b8d7c02b114b9447de5562796
EviamReset <eviam@resetcontrol.com>
2025-03-19 15:08:57 +0100

    control errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,67 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { Movies } from "./components/Movies.jsx";
 import { useMovies } from "./hooks/useMovie.js";
+import { useRef } from "react";
+
+function useSearch () {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+  
+    useEffect(() => {
+      if (isFirstInput.current) {
+        isFirstInput.current = search === "";
+        return;
+      }
+
+      if (search == "") {
+        setError("can't serach empty string");
+        return;
+      }
+  
+      if (search.length < 3) {
+        setError("search must be at least 3 characters long");
+        return;
+      }
+  
+      setError(null);
+    }, [search]);
+
+    return { search, updateSearch, error };
+}
 
 
-function App () {
+function App() {
   const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ search });
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
+  };
 
   return (
     <div className="page">
-
       <header>
         <h1>Movie Searcher</h1>
-          <form className="form">
-            <input placeholder="Avengers, Star Wars, The Matrix..." />
-            <button type="submit">Search</button>
-          </form>
-        
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
+            }}
+            onChange={handleChange}
+            value={search}
+            placeholder="Avengers, Star Wars, The Matrix..."
+          />
+          <button type="submit">Search</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
